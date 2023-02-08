d0280e24f81bac130a2a64c008d5ee86ecc2d5ea
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-02-07 17:59:31 -0700

    integrated okta login and logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Nav, Stack } from 'react-bootstrap'
+import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loggedIn, setLoggedIn] = useState(true)
 
   return (
-    <div className="App">
-
-    </div>
+    <Stack>
+      <Nav className='justify-content-between p-3'>
+        <Nav.Item>
+          <h3>Logo</h3>
+        </Nav.Item>
+        <Nav.Item>
+          <div>
+          <LoginButton>Login</LoginButton>
+          {loggedIn && <LogoutButton>Logout</LogoutButton>}
+          </div>
+        </Nav.Item>
+      </Nav>
+      <Stack gap={2} className='mt-5'>
+        <h1 className='text-center fw-bold' style={{fontSize: '6em'}}>Dyd</h1>
+        <h2 className='text-center text-secondary'>Share what you dyd.</h2>
+      </Stack>
+    </Stack>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Auth0Provider } from "@auth0/auth0-react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Auth0Provider
+    domain='dev-523gwn14iqnjtm83.us.auth0.com'
+    clientId='OUuk4XP2xZfXr85vt3GH1daPKwSFEZVy'
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    
+    
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
