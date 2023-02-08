bf2cbe28ee101f61bb762203c18f2067b61b4408
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-02-07 18:04:10 -0700

    Okta authentication working

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import { Nav, Stack } from 'react-bootstrap'
 import LoginButton from './components/LoginButton'
 import LogoutButton from './components/LogoutButton'
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(true)
+
+  const { isAuthenticated, isLoading } = useAuth0()
+
 
   return (
     <Stack>
@@ -23,6 +27,8 @@ function App() {
         <h1 className='text-center fw-bold' style={{fontSize: '6em'}}>Dyd</h1>
         <h2 className='text-center text-secondary'>Share what you dyd.</h2>
       </Stack>
+      {isLoading && <h2>Loading...</h2>}
+      <h2 className='text-center'>User is {isAuthenticated ? 'Authenticated' : 'Not Authenticated'}</h2>
     </Stack>
   )
 }
